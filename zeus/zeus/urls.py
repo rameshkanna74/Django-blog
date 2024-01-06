d62f78bbc52b817c894c082bc0f5234b33467820
unknown <rameshkanna@aurexus.com>
2024-01-06 18:30:18 +0530

    tailwind css

--- zeus/zeus/urls.py
+++ zeus/zeus/urls.py
@@ -21,7 +21,7 @@ from django.urls import include, path
 from .views import home_view
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path("aurexus/", admin.site.urls),
     path("", include("users.urls")),
     path("auth/", include("djoser.urls")),
     path("auth/", include("djoser.urls.jwt")),
