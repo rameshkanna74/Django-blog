61d66565264262bece7028016c89c5f75b261ece
rameshkanna74 <rameshkanna725@gmail.com>
2024-01-06 22:52:53 +0530

    tailwind

--- zeus/zeus/settings.py
+++ zeus/zeus/settings.py
@@ -12,12 +12,10 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 import os
 from pathlib import Path
 
-import dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# dotenv.read_dotenv()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
