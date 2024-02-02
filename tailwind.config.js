3b93c67be43e9e8d8ede3e069e788170b834f856
unknown <rameshkanna@aurexus.com>
2024-02-02 13:31:49 +0530

    Remove all files

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-
-module.exports = {
-  content: ["./web/**/*.{html,js}", "./node_modules/flowbite/**/*.js"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
--- web/core/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from .views import edit_document, home, index
-
-urlpatterns = [
-    path("", index),
-    path("accounts/profile/", home),
-    path("edit_document/<uuid:document_id>/", edit_document, name="edit_document"),
-]
